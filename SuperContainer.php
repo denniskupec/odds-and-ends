e524a147410a7c6c3f0eec3accb2781e73f7adbc
Dennis Kupec <denniskupec@gmail.com>
2015-04-06 16:40:46 -0500

    Create SuperContainer.php

---
+++ SuperContainer.php
@@ -0,0 +1,42 @@
+<?php
+
+class SuperContainer
+{
+	
+	protected $data;
+	
+	public function __construct()
+	{
+		$this->data = new SplObjectStorage;
+	}
+	
+	public function __get($key)
+	{
+		return $this->data->$key;
+	}
+	
+	public function __set($key, $value)
+	{
+		$this->data->$key = $value;
+	}
+	
+
+	public function __call($name, $arguments)
+	{
+		$c = create_function('$arguments, $that', $name);
+		return call_user_func($c, $arguments, $this);
+	}
+
+}
+
+$c = new SuperContainer;
+
+$c->poop = "dong";
+
+echo $c->{'
+
+	return "Hello, $arguments[0]" . PHP_EOL .
+			 "Poop" . $that->poop;
+
+'}("world!");
+
